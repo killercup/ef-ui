93355a190487801ed9d589419cbb428d1906fa42
Pascal Hertleif <killercup@gmail.com>
2015-01-19 22:26:32 +0100

    Show Store Fixes

--- src/data/index.js
+++ src/data/index.js
@@ -1,3 +1,5 @@
 module.exports = require('./bus');
 
 require('./auth');
+require('./shows');
+
--- src/data/shows/fetch.js
+++ src/data/shows/fetch.js
@@ -1,3 +1,4 @@
+var l = require('lodash');
 var bus = require('../bus');
 var API = require('../api');
 
@@ -6,18 +7,25 @@ function list() {
   .then(function (res) {
     bus.dispatch({type: 'SHOWS_FETCHED', data: res.body.shows});
   })
-  .then(function (err) {
+  .catch(function (err) {
     bus.dispatch({type: 'SHOWS_FAILURE', data: err});
   });
 }
 
 function detail(data) {
   var id = data.id;
-  return API.request({url: '/shows/' + id})
+  return API.request({url: '/shows/' + id, query: {include: 'episodes'}})
   .then(function (res) {
-    bus.dispatch({type: 'SHOW_FETCHED', data: res.body.shows});
+    var show;
+    if (l.isObject(res.body.shows)) {
+      show = res.body.shows;
+    }
+    if (l.isArray(res.body.episodes) && l.isNumber(res.body.episodes[0])) {
+      show.episodes = res.body.episodes;
+    }
+    bus.dispatch({type: 'SHOW_FETCHED', data: show});
   })
-  .then(function (err) {
+  .catch(function (err) {
     bus.dispatch({type: 'SHOW_FAILURE', data: err});
   });
 }
