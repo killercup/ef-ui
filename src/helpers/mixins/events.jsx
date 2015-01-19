0890931f95ff63730b6833fa7d63930f894bdc70
Pascal Hertleif <killercup@gmail.com>
2015-01-20 00:21:46 +0100

    Fix Event Handling Mixin

--- src/helpers/mixins/events.jsx
+++ src/helpers/mixins/events.jsx
@@ -6,14 +6,30 @@ module.exports = {
   },
 
   componentDidMount() {
+    this._fancyEvents = this._fancyEvents || {};
+
     Object.keys(this.events || {}).forEach((event) => {
-      getEvents(event).onValue(this.events[event].bind(this));
+      var stream = getEvents(event);
+
+      this._fancyEvents[event] = {
+        action: this.events[event].bind(this)
+      };
+
+      // Since each time we call `getEvents`, a new stream gets created, we need
+      // to create and save an unsub handler as well. This should not cause any
+      // problems, the number of event handlers in a component should be quite
+      // small (<10).
+      this._fancyEvents[event].unsub = () => {
+        stream.offValue(this._fancyEvents[event].action);
+      };
+
+      stream.onValue(this._fancyEvents[event].action);
     });
   },
 
   componentWillUnmount() {
-    Object.keys(this.events || {}).forEach((event) => {
-      getEvents(event).offValue(this.events[event].bind(this));
+    Object.keys(this._fancyEvents || {}).forEach((event) => {
+      this._fancyEvents[event].unsub();
     });
   }
 };
