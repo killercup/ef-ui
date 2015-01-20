b4e1ae9ec7dd3628ab06e2af25fe01116010aa49
Pascal Hertleif <killercup@gmail.com>
2015-01-20 18:46:37 +0100

    Refactor Episode Component

--- src/components/episode/index.jsx
+++ src/components/episode/index.jsx
@@ -13,22 +13,28 @@ module.exports = React.createClass({
   ],
 
   propTypes: {
+    id: ReactProps.require({type: 'number', min: 1}),
     name: ReactProps.require({type: 'string', pattern: 'lorem.sentence'}),
     show_id: ReactProps.require({type: 'number', min: 1}),
     number: ReactProps.require({type: 'number', min: 0, max: 1024}),
     season: ReactProps.require({type: 'number', min: 0, max: 1024}),
+    vote: ReactProps.optional({
+      id: {type: 'number', min: 1},
+      rating: {type: 'number', min: 1, max: 3}
+    }),
   },
 
   render() {
     var k = this.getKeyHelper();
+    var p = this.props;
 
     return (
       <article className="Episode">
         <div {...k('title')}>
-          S<span {...k('season')}>{padDigits(2, this.props.season)}</span>
-          E<span {...k('number')}>{padDigits(2, this.props.number)}</span>
+          S<span {...k('season')}>{padDigits(2, p.season)}</span>
+          E<span {...k('number')}>{padDigits(2, p.number)}</span>
           &nbsp;
-          <span {...k('name')}>{this.props.name}</span>
+          <span {...k('name')}>{p.name}</span>
         </div>
       </article>
     );
